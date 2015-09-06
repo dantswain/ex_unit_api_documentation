2155dc65e401ef5d20c8047f8eae7de818cea7cf
Dan Swain <dan.t.swain@gmail.com>
2015-09-06 15:18:24 -0400

    Remove endpoint, extra parts of router
    Determined not needed by trial and error

--- web/router.ex
+++ web/router.ex
@@ -1,21 +1,3 @@
 defmodule ExUnitApiDocumentation.Router do
   use ExUnitApiDocumentation.Web, :router
-
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
-  scope "/docs", ExUnitApiDocumentation do
-    pipe_through :browser
-
-    get "/", DocsController, :index
-  end
 end
